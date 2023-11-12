d9dd068e893e51edaee92ca2fdf78528dc0deff3
Sardor <sardoribodov07@gmail.com>
2023-11-12 10:43:20 +0500

    deployed ready

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -34,7 +34,7 @@ const Results = () => {
         <h3 className="font-bold text-2xl text-center my-3">
           {data?.category_id == 1 ? "Lights" : "Air Conditioner"}
         </h3>
-        <div className="flex flex-col gap-y-5 text-[25px] text-center">
+        <div className="flex flex-col	text-[25px] text-center">
           <h3>
             Count: <span className="font-bold">{data?.count}</span>
           </h3>
@@ -51,28 +51,28 @@ const Results = () => {
         <div className="mt-3 flex justify-center gap-x-32">
           <div>
             <h3 className="font-semibold">
-              Daily you spend
+              Daily you spend:
               <span className="font-bold">
                 {parseInt(data?.your?.one_day)} sum
               </span>{" "}
               on light bulbs
             </h3>
             <h3 className="font-semibold">
-              In a week you spend
+              In a week you spend: 
               <span className="font-bold">
                 {parseInt(data?.your?.one_week)} sum
               </span>{" "}
               on light bulbs
             </h3>
             <h3 className="font-semibold">
-              In a month you spend
+              In a month you spend:
               <span className="font-bold">
                 {parseInt(data?.your?.one_month)} sum
               </span>{" "}
               on light bulbs
             </h3>
             <h3 className="font-semibold">
-              In a year you spend
+              In a year you spend:
               <span className="font-bold">
                 {parseInt(data?.your?.one_year)} sum
               </span>{" "}
@@ -82,18 +82,35 @@ const Results = () => {
         </div>
       </div>
       <div className="p-4  mt-3 shadow-lg w-1/2 mx-auto bg-green-200">
-        <h2 className="text-center font-bold text-3xl">
+        <h2 className="text-center font-bold text-2xl">
           If you use our technology you will save:
         </h2>
-        <h3 className="my-4 text-center font-semibold text-2xl">
-          <span className="font-bold text-4xl">
-            {data?.your?.ten_year - data?.we_calculate?.ten_year} sum
+				<h3 className="my-4 text-center font-semibold text-xl">
+          <span className="font-bold text-2xl">
+            {parseInt(data?.your?.one_month - data?.we_calculate?.one_month)} sum
+          </span>{" "}
+          in 10 years
+        </h3>
+        <h3 className="my-4 text-center font-semibold text-xl">
+          <span className="font-bold text-2xl">
+            {parseInt(data?.your?.one_year - data?.we_calculate?.one_year)} sum
+          </span>{" "}
+          in a year
+        </h3>
+        <h3 className="my-4 text-center font-semibold text-xl">
+          <span className="font-bold text-2xl">
+            {parseInt(data?.your?.ten_year - data?.we_calculate?.ten_year)} sum
           </span>{" "}
           in 10 years
         </h3>
       </div>
       <div className="flex justify-center mt-3 absolute left-4">
-        <Button onClick={() => navigate("/details")} className="bg-blue-600 text-white">Back to details</Button>
+        <Button
+          onClick={() => navigate("/details")}
+          className="bg-blue-600 text-white"
+        >
+          Back to details
+        </Button>
       </div>
     </div>
   );
